03ca060bf3accbe43bfa1e85a4db9a852939db19
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-05 16:11:21 +0200

    Update range and filtering in IntStream
    Modified IntStream in LabCompStreamApp to improve output readability. Starting range changed from 1 to 0 and ending range from 20 to 26 to include the entire alphabet. 'mapToObj' now formats strings according to alphabetical order rather than numeric. Further filtering has been implemented to limit output to characters 'e' through 'o'. This change clarifies the purpose of the code and the resulting output for users.

--- src/it/unibs/pajc/LabCompStreamApp.java
+++ src/it/unibs/pajc/LabCompStreamApp.java
@@ -6,8 +6,10 @@ public class LabCompStreamApp {
     public static void main(String[] args) {
 
         IntStream
-                .range(1, 20)
-                .mapToObj(x -> String.format("File_%02x", x))
-                .forEach(System.out::println);
+                .range(0, 26)
+                .mapToObj(x -> String.format("%c_%d", 'a'+x, x))
+                .filter((x) -> (int)x.charAt(0) >= 'e')
+                .filter((x) -> (int)x.charAt(0) <= 'o')
+                .forEach((x) -> System.out.printf("%c\n", x.charAt(0)));
     }
 }
